cc89bf4a3538ebbe6fb09ffbcae4bb9a42a05b2c
Riley H. Mæhle <oscarhm@student.matnat.uio.no>
2020-08-27 13:20:22 +0200

    Now tests addition of floats

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,18 @@
 import calculator
 
+#Creates an error message for a typical assert statement
+def message(expected, recieved):
+    return f"Expected {expected}, got {num}"
+
 def test_add_excercise_1():
     num = calculator.add(1, 2)
-    expected = 3
-    msg = f"Expected {expected}, got {num}"
-    assert num==expected, msg
+    expected = 3 
+    assert num==expected, message(expected, num)
+
+def test_add_excercise_2():
+    num = calculator.add(0.1, 0.2)
+    expected = 0.3
+    tolerance = 10**-12
+    assert abs(num - expected) < tolerance, message(expected, num)
+
+    
