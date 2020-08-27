280926d382ea179528bc65759c9aa9d2eb41a190
Riley H. Mæhle <oscarhm@student.matnat.uio.no>
2020-08-27 13:38:59 +0200

    Implemented factorial function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,11 @@
 
+#Implements addition
 def add(x, y):
     return x + y
+
+#Implements the factorial function n! for integers n
+def factorial(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    return fact
