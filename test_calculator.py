46fda6484099b117fd49a2e5d003a0f54c5526ff
Riley H. Mæhle <oscarhm@student.matnat.uio.no>
2020-08-28 16:30:21 +0200

    Parametrized excercise 4 tests

--- test_calculator.py
+++ test_calculator.py
@@ -32,52 +32,51 @@ def test_add_excercise_3(x, y, expected):
     assert string == expected, message(expected, string)
 
 #Tests the factorial function for 4 inputs against math.factorial
-def test_factorial_excercise_4():
-    for n in (0, 1, 5, 10):
-        num = calculator.factorial(n)
-        expected = math.factorial(n)
-        assert num==expected, message(expected, num)
+@pytest.mark.parametrize("n", (0, 1, 5, 10))
+def test_factorial_excercise_4(n):
+    num = calculator.factorial(n)
+    expected = math.factorial(n)
+    assert num==expected, message(expected, num)
 
-#Tests the sin function for 4 inpusts against math.sin
-def test_sin_excercise_4():
+#Tests the sin function for 4 inputs against math.sin
+@pytest.mark.parametrize("x", (0, 0.1, 1, math.pi))
+def test_sin_excercise_4(x):
     N = 50
-    for x in (0, 0.1, 1, math.pi):
-        num = calculator.sin(x, N)
-        expected = math.sin(x)
-        assert abs(num - expected) < tolerance, message(expected, num)
+    num = calculator.sin(x, N)
+    expected = math.sin(x)
+    assert abs(num - expected) < tolerance, message(expected, num)
 
 #Tests the divide function for 4 sets of inputs
-def test_divide_excercise_4():
-    args = ((0, 1), (100, 10), (7, 5), (5, 7))
-    expected = (0, 10, 1.4, 0.714285714285714)
-    for i in range(4):
-        num = calculator.divide(args[i][0], args[i][1])
-        assert abs(num - expected[i] < tolerance), message(expected[i], num) 
+@pytest.mark.parametrize("x, y, expected", \
+    ((0, 1, 0), (120.75, 10.5, 11.5), (7, 5, 1.4), (5, 7, 0.714285714285714)))
+def test_divide_excercise_4(x, y, expected):
+    num = calculator.divide(x, y)
+    assert abs(num - expected < tolerance), message(expected, num) 
 
 #Tests the exp function for 4 inputs against math.exp
-def test_exp_excercise_4():
+@pytest.mark.parametrize("x", (-2, 0, 0.5, 4))
+def test_exp_excercise_4(x):
     N = 50
-    for x in (-2, 0, 0.5, 4):
-        num = calculator.exp(x, N)
-        expected = math.exp(x)
-        assert abs(num - expected) < tolerance, message(expected, num)
+    num = calculator.exp(x, N)
+    expected = math.exp(x)
+    assert abs(num - expected) < tolerance, message(expected, num)
 
 #Tests the combination function for 4 inputs against math.comb
-def test_comb_excercise_4():
+@pytest.mark.parametrize("i", (0, 25, 50, 60))
+def test_comb_excercise_4(i):
     n = 50
-    for i in (0, 25, 50, 60):
-        num = calculator.comb(n, i)
-        expected = math.comb(n, i)
-        assert num == expected, message(expected, num)
+    num = calculator.comb(n, i)
+    expected = math.comb(n, i)
+    assert num == expected, message(expected, num)
 
 #Tests the comb function for numbers too large to be stored as ints
-def test_comb_large_args_excercise_4():
+@pytest.mark.parametrize("i", (0, 50, 250, 500))
+def test_comb_large_args_excercise_4(i):
     n = 500
-    for i in (0, 50, 250, 500):
-        num = calculator.comb(n, i)
-        expected = math.comb(n, i)
-        #Since the numbers are quite large, the relative error is more interesting than the absolute error
-        assert abs(num/expected - 1) < tolerance, message(expected, num)
+    num = calculator.comb(n, i)
+    expected = math.comb(n, i)
+    #Since the numbers are quite large, the relative error is more interesting than the absolute error
+    assert abs(num/expected - 1) < tolerance, message(expected, num)
 
 #Checks that the add function raises a TypeError for str+int
 def test_add_raises_TypeError_for_string_plus_integer_excercise_5():
